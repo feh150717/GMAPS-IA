cded3eeee117fb41d24e8de589ae11c9407dcf13
feh150717 <fehfaustino150717@gmail.com>
2025-12-12 17:00:58 -0300

    Update whatsapp.py

--- utils/whatsapp.py
+++ utils/whatsapp.py
@@ -1,8 +1,12 @@
+import os
 import requests
-from utils.config import API_URL
+
+ZAPI_INSTANCE = os.getenv("ZAPI_INSTANCE")
+ZAPI_TOKEN = os.getenv("ZAPI_TOKEN")
+
+API_URL = f"https://api.z-api.io/instances/{ZAPI_INSTANCE}/token/{ZAPI_TOKEN}"
 
 def enviar_mensagem(telefone, texto):
-
     url = f"{API_URL}/send-text"
 
     payload = {
@@ -10,13 +14,10 @@ def enviar_mensagem(telefone, texto):
         "message": texto
     }
 
-    response = requests.post(url, json=payload)
-
     try:
+        response = requests.post(url, json=payload)
+        print("➡️ Enviando mensagem para:", telefone)
+        print("➡️ Texto:", texto)
         return response.json()
-    except:
-        return {
-            "error": "Erro ao interpretar resposta",
-            "status_code": response.status_code,
-            "text": response.text
-        }
+    except Exception as e:
+        return {"error": str(e)}

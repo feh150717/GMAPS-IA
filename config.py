937fe7b9c055697abf488230f1e777ce41e41b6a
feh150717 <fehfaustino150717@gmail.com>
2025-12-12 01:55:18 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # Configurações da Z-API
 
-API_ID = "3EB916797636317CC1202ABC0BC379A8"   # ID da instância
-API_TOKEN = "50349354C6FA1AE4807546D1"        # Token da instância
+API_ID = "3EB916797636317CC1202ABC0BC379A8"      # ID da instância
+API_TOKEN = "50349354C6F1AE4807546D1"           # Token da instância
 
-# URL base da API
+# URL base para envio de mensagens
 API_URL = f"https://api.z-api.io/instances/{API_ID}/token/{API_TOKEN}"

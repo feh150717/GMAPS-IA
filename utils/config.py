9ec1a76051a3968cc7c6d576025cd02209714728
feh150717 <fehfaustino150717@gmail.com>
2025-12-12 17:45:29 -0300

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -3,4 +3,4 @@ import os
 API_ID = os.getenv("ZAPI_INSTANCE")
 API_TOKEN = os.getenv("ZAPI_TOKEN")
 
-API_URL = f"https://api.z-api.io/instances/{API_ID}/token/{API_TOKEN}
+API_URL = f"https://api.z-api.io/instances/{API_ID}/token/{API_TOKEN}/send-text"

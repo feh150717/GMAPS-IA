5bdf459d8b2e70874a7beb85ccdc21c81047d268
feh150717 <fehfaustino150717@gmail.com>
2025-12-12 00:59:58 -0300

    Create whatsapp.py

---
+++ utils/whatsapp.py
@@ -0,0 +1,20 @@
+import requests
+from config import ZAPI_INSTANCE, ZAPI_TOKEN
+
+BASE_URL = f"https://api.z-api.io/instances/{ZAPI_INSTANCE}/token/{ZAPI_TOKEN}"
+
+def enviar_mensagem(numero, mensagem):
+    try:
+        url = f"{BASE_URL}/send-text"
+
+        payload = {
+            "phone": numero,
+            "message": mensagem
+        }
+
+        response = requests.post(url, json=payload)
+
+        return response.json()
+
+    except Exception as e:
+        return {"error": str(e)}
